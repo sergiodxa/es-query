2efe6412af313c7b15f179d2da42fd0f74e82b2f
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-28 20:46:05 -0300

    project started

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var eslint     = require('gulp-eslint');
+var gulp       = require('gulp');
+
+// rutas de donde leer archivos en las distintas tareas
+// y donde dejar los archivos generados
+var paths = {
+  src: {
+    eslint: ['./modules/**/*.js', './index.js'],
+  }
+}
+
+gulp.task('eslint', function () {
+  return gulp
+    .src(paths.src.eslint)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('watch', function () {
+  gulp.watch([paths.src.eslint], ['eslint']);
+});
+
+gulp.task('default', ['eslint', 'watch']);
