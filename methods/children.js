6afaac16f474e12c98bbc4860b4516edf77715c7
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-28 21:30:13 -0300

    fixed errors in children()#30

--- methods/children.js
+++ methods/children.js
@@ -1,12 +1,12 @@
 function children () {
-  let children = [];
-  for (let i = this.children.length; i--;) {
-    // Skip comment nodes on IE8
-    if (this.children[i].nodeType != 8) {
-      children.unshift(this.children[i]);
+  let childrenElements = [];
+
+  for (let i = 0, len = this.children.length; i < len; i++) {
+    if (this.children[i].nodeType !== 8) {
+      childrenElements.unshift(this.children[i]);
     }
   }
-  return children;
+  return childrenElements;
 }
 
 export default children;
